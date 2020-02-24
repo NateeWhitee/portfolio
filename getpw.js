d6bdb25617fb5575e2ff0e63ef1a1532298e1098
NateeWhitee <59496066+NateeWhitee@users.noreply.github.com>
2020-02-24 15:03:05 -0600

    Add files via upload

---
+++ getpw.js
@@ -0,0 +1,6 @@
+let pw = prompt('In order to access this page, you must input a password')
+if (pw === 'Nw112358!') {
+    window.location.href='contact.html';
+} else {
+    alert('Incorrect reload webpage to try again');
+}
